b2c7ee5cea39a8d22d93413361a39bbebe90014b
Lucas Patrick <tech.patrick.lsilva@gmail.com>
2026-02-08 17:23:17 -0300

    hero section

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,8 +12,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang="pt-BR">
+      <body className="min-h-svh dark antialiased">{children}</body>
     </html>
   );
 }
